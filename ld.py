cf06243f7e79f1494696dc77ddde979324f100f2
presbrey <presbrey+git@gmail.com>
2013-03-21 17:49:48 -0400

    add application/sparql-update support

--- ld.py
+++ ld.py
@@ -1,11 +1,18 @@
-#!/usr/bin/python
-from RDF import *
+#!/usr/bin/env python
+from RDF import NS, Uri, Node, Statement, Storage, Model, SPARQLQuery, Serializer, Parser
+
 class ns(object):
     rdf  = NS('http://www.w3.org/1999/02/22-rdf-syntax-ns#')
     rdfs = NS('http://www.w3.org/2000/01/rdf-schema#')
+    owl  = NS('http://www.w3.org/2002/07/owl#')
     acl  = NS('http://www.w3.org/ns/auth/acl#')
     cert = NS('http://www.w3.org/ns/auth/cert#')
     stat = NS('http://www.w3.org/ns/posix/stat#')
+    foaf = NS('http://xmlns.com/foaf/0.1/')
+    dc   = NS('http://purl.org/dc/elements/1.1/')
+    cc   = NS('http://creativecommons.org/ns#')
+
+from rdflib_sparql import processor as sparqlUpdate
 
 from flask import Flask, Response, request, abort
 app = Flask(__name__)
@@ -30,10 +37,11 @@ class Graph(object):
 
     def __init__(self, p, base_uri=None):
         self._base = base_uri or request.base_url
-        self._f0 = 'turtle'
-        self._p0 = os.path.abspath(p)
-        #if p and p[-1]=='/': self._p0 += '/'
-        self._p = self._p0
+        self._f = self._f0 = 'turtle'
+        self._p = os.path.abspath(p)
+        if p and p[-1]=='/' and (not self._p or self._p[-1]!='/'):
+            self._p += '/'
+        self._p0 = self._p
         if not self.exists():
             for k in self.TYPEMAP:
                 if self.exists(p + '.' + k):
@@ -78,17 +86,31 @@ class Graph(object):
         p = Parser(name=name, mime_type=mime_type)
         assert p.parse_string_into_model(self._g, s, self._base, handler=self._h)
 
+    def update(self, s, name=None, mime_type=None):
+        if mime_type == 'application/sparql-update':
+            for op in sparqlUpdate.translateUpdate(sparqlUpdate.parseUpdate(s)):
+                if op.name is 'InsertData':
+                    g = Graph('sparql:')
+                    for elt in op.triples:
+                        g.append(' '.join([x.n3() for x in elt]), name='turtle')
+                    for x in g._g:
+                        self._g.append(x)
+                elif op.name is 'DeleteData':
+                    g = Graph('sparql:')
+                    for elt in op.triples:
+                        g.append(' '.join([x.n3() for x in elt]), name='turtle')
+                    for x in g._g:
+                        if x in self._g:
+                            del self._g[x]
+        else:
+            return self.append(s, name, mime_type)
+
     def toString(self, f):
         s = Serializer(name=f)
         s.set_feature(Uri('http://feature.librdf.org/raptor-writeBaseURI'), Node(literal='0')._node)
-        s.set_namespace('rdfs', Uri('http://www.w3.org/2000/01/rdf-schema#'))
-        s.set_namespace('acl', Uri('http://www.w3.org/ns/auth/acl#'))
-        s.set_namespace('cert', Uri('http://www.w3.org/ns/auth/cert#'))
-        s.set_namespace('stat', Uri('http://www.w3.org/ns/posix/stat#'))
-        s.set_namespace('foaf', Uri('http://xmlns.com/foaf/0.1/'))
-        s.set_namespace('dc', Uri('http://purl.org/dc/elements/1.1/'))
-        s.set_namespace('cc', Uri('http://creativecommons.org/ns#'))
-        s.set_namespace('owl', Uri('http://www.w3.org/2002/07/owl#'))
+        for k, v in list(ns.__dict__.items()):
+            if type(v) is NS:
+                s.set_namespace(k, Uri(v._prefix))
         return s.serialize_model_to_string(self._g, base_uri=self._base)
 
     def save(self):
@@ -111,7 +133,7 @@ class Graph(object):
                     return Response(r.to_string('http://www.w3.org/ns/formats/SPARQL_Results_'+k.upper(), base_uri=self._base))
         return Response(r.to_string('http://www.w3.org/ns/formats/SPARQL_Results_JSON', base_uri=self._base))
 
-    def __call__(self):
+    def __call__(self, status=None):
         f = 'turtle'
         m = 'text/turtle'
         for mtype, q in request.accept_mimetypes:
@@ -120,7 +142,7 @@ class Graph(object):
                 f = self.TYPEMAP[elt[1]]
                 m = mtype
                 break
-        return Response(self.toString(f), mimetype=m, headers=self.info())
+        return Response(self.toString(f), status=status, headers=self.info(), mimetype=m)
 
 @app.route('/')
 @app.route('/<path:graph>')
@@ -131,23 +153,19 @@ def httpRead(graph=''):
     g.load()
     return g()
 
-@app.route('/<path:path>/')
-def httpIndex(path='./'):
-    g = Graph(path)
-    if not g.exists():
-        return '', 404, {}
-    return g()
-
 @app.route('/<path:graph>', methods=('POST', 'PUT'))
 def httpWrite(graph):
     d = request.data
-    if request.content_type and d:
+    if request.content_type:
         g = Graph(graph)
         if request.method != 'PUT':
-            g.load()
-        g.append(d, mime_type=request.content_type)
+            if g.exists():
+                g.load()
+        if d:
+            g.update(d, mime_type=request.content_type)
         g.save()
-    return '', 200, g.info()
+        return '', 204, g.info()
+    return '', 415, g.info()
 
 @app.route('/<path:graph>', methods=('DELETE',))
 def httpDELETE(graph):
@@ -157,11 +175,18 @@ def httpDELETE(graph):
     g.unlink()
     if g.exists():
         return '', 409, {}
-    return '', 200, g.info()
+    return '', 204, g.info()
 
-@app.route('/<path:path>/', methods=('MKCOL',))
-def httpMKCOL(path):
-    os.makedirs(path)
+@app.route('/<path:graph>', methods=('MKCOL',))
+def httpMKCOL(graph):
+    g = Graph(graph)
+    if g.exists():
+        return '', 409, {}
+    os.makedirs(graph)
+    if not g.exists():
+        return '', 404, {}
+    g.load()
+    return g(status=201)
 
 def application(app):
     def handler(env, respond):
